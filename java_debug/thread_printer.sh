de02c11734be74bfd26de5633993e703f7ec57a0
PanSzelescik <panszelescik@gmail.com>
2021-09-21 18:16:28 +0200

    Support variable

--- java_debug/thread_printer.sh
+++ java_debug/thread_printer.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+re='^[0-9]+$'
+if ! [[ $IBC_DEBUG_STALL_REPORT_INTERVAL_SECONDS =~ $re ]]; then
+  IBC_DEBUG_STALL_REPORT_INTERVAL_SECONDS=10
+fi
+
 while :
 do
-  sleep 10
+  sleep IBC_DEBUG_STALL_REPORT_INTERVAL_SECONDS
   mkdir -p /home/container/stall-reports
   pid=$(ps ax | grep -i "java " | awk 'NR==1{print $1}')
   jcmd $pid Thread.print > "/home/container/stall-reports/stall_$(date +"%Y-%m-%d_%H.%M.%S")"
