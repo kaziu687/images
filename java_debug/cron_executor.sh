306f703059c295f1584148e6be600ce8efcf14a2
PanSzelescik <panszelescik@gmail.com>
2021-09-21 17:53:32 +0200

    Fix

--- java_debug/cron_executor.sh
+++ java_debug/cron_executor.sh
@@ -4,7 +4,7 @@ while :
 do
   sleep 10
   mkdir -p /home/container/stall-reports
-  jcmd -l > "/home/container/stall-reports/jcmd_$(date +"%Y-%m-%d_%H.%M.%S")" 2> "/home/container/stall-reports/jcmd_$(date +"%Y-%m-%d_%H.%M.%S")_error"
+  ps ax > "/home/container/stall-reports/jcmd_$(date +"%Y-%m-%d_%H.%M.%S")"
   pid=$(jcmd -l | grep "[0-9] java *" | awk '{print $1}')
   jcmd $pid Thread.print > "/home/container/stall-reports/stall_$(date +"%Y-%m-%d_%H.%M.%S")"
 done
