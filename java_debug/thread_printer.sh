475c632ac7a624dbb2bdb4b6393957784eadc59b
PanSzelescik <panszelescik@gmail.com>
2021-09-21 18:17:51 +0200

    Fix

--- java_debug/thread_printer.sh
+++ java_debug/thread_printer.sh
@@ -7,7 +7,7 @@ fi
 
 while :
 do
-  sleep IBC_DEBUG_STALL_REPORT_INTERVAL_SECONDS
+  sleep $IBC_DEBUG_STALL_REPORT_INTERVAL_SECONDS
   mkdir -p /home/container/stall-reports
   pid=$(ps ax | grep -i "java " | awk 'NR==1{print $1}')
   jcmd $pid Thread.print > "/home/container/stall-reports/stall_$(date +"%Y-%m-%d_%H.%M.%S")"
