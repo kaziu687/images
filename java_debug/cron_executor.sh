26e5adc548fb675e585b80344eabab7ad352fc29
PanSzelescik <panszelescik@gmail.com>
2021-09-21 17:23:44 +0200

    jdk

--- java_debug/cron_executor.sh
+++ java_debug/cron_executor.sh
@@ -1,13 +1,9 @@
 #!/bin/bash
-mkdir -p /home/container/stall-reports
 
-sleep 60
-jcmd > /home/container/test.txt
-pid=$(jcmd | grep "[0-9] java *" | awk '{print $1}')
-echo $pid
-
-if [ -z ${pid+x} ]; then
-  echo "PID unset!"
-else
+while :
+do
+  sleep 10
+  mkdir -p /home/container/stall-reports
+  pid=$(jcmd | grep "[0-9] java *" | awk '{print $1}')
   jcmd $pid Thread.print > "/home/container/stall-reports/stall_report_$(date +"%Y-%m-%d_%H.%M.%S")"
-fi
+done
