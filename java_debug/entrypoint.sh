337b56c51a5923a1b18b5ca1fbe0eeedc575060c
PanSzelescik <panszelescik@gmail.com>
2021-09-21 17:59:51 +0200

    Fix

--- java_debug/entrypoint.sh
+++ java_debug/entrypoint.sh
@@ -47,7 +47,7 @@ PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat
 printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
 
 bash
-/utils/./cron_executor.sh &
+/utils/./thread_printer.sh &
 
 # shellcheck disable=SC2086
 exec env ${PARSED}
--- java_debug/thread_printer.sh
+++ java_debug/thread_printer.sh
@@ -4,7 +4,7 @@ while :
 do
   sleep 10
   mkdir -p /home/container/stall-reports
-  ps ax > "/home/container/stall-reports/jcmd_$(date +"%Y-%m-%d_%H.%M.%S")"
-  pid=$(jcmd -l | grep "[0-9] java *" | awk '{print $1}')
+  #pid=$(jcmd -l | grep "[0-9] java *" | awk '{print $1}')
+  pid=1
   jcmd $pid Thread.print > "/home/container/stall-reports/stall_$(date +"%Y-%m-%d_%H.%M.%S")"
 done
