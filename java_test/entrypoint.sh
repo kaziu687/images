fa4b8c1dacc46d9df75e0370fd2bf15a11da362b
PanSzelescik <panszelescik@gmail.com>
2021-10-07 15:19:50 +0200

    Fix and add if

--- java_test/entrypoint.sh
+++ java_test/entrypoint.sh
@@ -52,21 +52,23 @@ if [ ! -f "${SERVER_JARFILE}" ]; then
 fi
 
 # ANTI-BOT
-mkdir -p /home/container/dodatkowe-lobby/plugins
-cd /home/container/dodatkowe-lobby/plugins || exit 1
-curl -LSo AntiBot.jar https://github.com/PanSzelescik/images/raw/main/java_test/AntiBot.jar
-cd /home/container/dodatkowe-lobby || exit 1
-curl -LSo flamecord.jar https://github.com/PanSzelescik/images/raw/main/java_test/flamecord.jar
+if [[ ${IBC_CHECKBOX_INTERNAL_LOBBY} == "true" ]]; then
+    mkdir -p /home/container/dodatkowe-lobby/plugins
+    cd /home/container/dodatkowe-lobby/plugins || exit 1
+    curl -LSo AntiBot.jar https://github.com/PanSzelescik/images/raw/main/java_test/AntiBot.jar
+    cd /home/container/dodatkowe-lobby || exit 1
+    curl -LSo flamecord.jar https://github.com/PanSzelescik/images/raw/main/java_test/flamecord.jar
 
-#INTERNAL_LOBBY_PORT=SERVER_PORT
-INTERNAL_LOBBY_PORT=35007
-SERVER_PORT=25565
+    #INTERNAL_LOBBY_PORT=SERVER_PORT
+    INTERNAL_LOBBY_PORT=35007
+    SERVER_PORT=25565
 
-sed -e "s/query_port: \([0-9]\+\)/query_port: $INTERNAL_SERVER_PORT/g" -e "s/host: 0.0.0.0:\([0-9]\+\)/host: 0.0.0.0:$INTERNAL_SERVER_PORT/g" config.yml > bh_config.yml
-mv bh_config.yml config.yml
+    sed -e "s/query_port: \([0-9]\+\)/query_port: $INTERNAL_LOBBY_PORT/g" -e "s/host: 0.0.0.0:\([0-9]\+\)/host: 0.0.0.0:$INTERNAL_LOBBY_PORT/g" config.yml > bh_config.yml
+    mv bh_config.yml config.yml
 
-java -Xms128M -Xmx512M -jar flamecord.jar &
-cd /home/container || exit 1
+    java -Xms128M -Xmx512M -jar flamecord.jar &
+    cd /home/container || exit 1
+fi
 # ANTI-BOT
 
 # shellcheck disable=SC2086
