0703ffe691764bf17c59cae5aff82bf39adffa2b
PanSzelescik <panszelescik@gmail.com>
2021-10-07 19:20:09 +0200

    While test

--- java_test/antibot.sh
+++ java_test/antibot.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-if [[ ${IBC_CHECKBOX_INTERNAL_LOBBY} == "true" ]]; then
-    mkdir -p /home/container/dodatkowe-lobby/plugins
-    cd /home/container/dodatkowe-lobby/plugins || exit 1
-    curl -LSo AntiBot.jar https://github.com/PanSzelescik/images/raw/main/java_test/AntiBot.jar
-    cd /home/container/dodatkowe-lobby || exit 1
-    curl -LSo flamecord.jar https://github.com/PanSzelescik/images/raw/main/java_test/flamecord.jar
+while :
+do
+    if [[ ${IBC_CHECKBOX_INTERNAL_LOBBY} == "true" ]]; then
+        mkdir -p /home/container/dodatkowe-lobby/plugins
+        cd /home/container/dodatkowe-lobby/plugins || exit 1
+        curl -LSo AntiBot.jar https://github.com/PanSzelescik/images/raw/main/java_test/AntiBot.jar
+        cd /home/container/dodatkowe-lobby || exit 1
+        curl -LSo flamecord.jar https://github.com/PanSzelescik/images/raw/main/java_test/flamecord.jar
 
-    INTERNAL_LOBBY_PORT=$SERVER_PORT
-    SERVER_PORT=25565
+        INTERNAL_LOBBY_PORT=$SERVER_PORT
+        SERVER_PORT=25565
 
-    if [ ! -f /home/container/dodatkowe-lobby/config.yml ]; then
-        echo -e "listeners:\r- host: 0.0.0.0:${INTERNAL_LOBBY_PORT}\r  query_enabled: true\r  query_port: ${INTERNAL_LOBBY_PORT}\r  motd: 'Serwer hostowany przez §aBedrock§fHost.pl'" > /home/container/dodatkowe-lobby/config.yml
-    else
-        sed -e "s/query_port: \([0-9]\+\)/query_port: $INTERNAL_LOBBY_PORT/g" -e "s/host: 0.0.0.0:\([0-9]\+\)/host: 0.0.0.0:$INTERNAL_LOBBY_PORT/g" config.yml > bh_config.yml
-        mv bh_config.yml config.yml
+        if [ ! -f /home/container/dodatkowe-lobby/config.yml ]; then
+            echo -e "listeners:\r- host: 0.0.0.0:${INTERNAL_LOBBY_PORT}\r  query_enabled: true\r  query_port: ${INTERNAL_LOBBY_PORT}\r  motd: 'Serwer hostowany przez §aBedrock§fHost.pl'" > /home/container/dodatkowe-lobby/config.yml
+        else
+            sed -e "s/query_port: \([0-9]\+\)/query_port: $INTERNAL_LOBBY_PORT/g" -e "s/host: 0.0.0.0:\([0-9]\+\)/host: 0.0.0.0:$INTERNAL_LOBBY_PORT/g" config.yml > bh_config.yml
+            mv bh_config.yml config.yml
+        fi
+
+        printf "\033[1m\033[33m[BedrockHost.pl - AntiBot]: \033[0mUruchamianie AntiBota...\n"
+        sleep 10
+        #java -Xms128M -Xmx512M -jar flamecord.jar &>/dev/null
     fi
-
-    printf "\033[1m\033[33m[BedrockHost.pl - AntiBot]: \033[0mUruchamianie AntiBota...\n"
-    java -Xms128M -Xmx512M -jar flamecord.jar &>/dev/null
-    cd /home/container || exit 1
-fi
+done
