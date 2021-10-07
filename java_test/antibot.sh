c5375888ce7776a4693ff6f17e969d41a0c5006a
PanSzelescik <panszelescik@gmail.com>
2021-10-07 20:13:42 +0200

    Config

--- java_test/antibot.sh
+++ java_test/antibot.sh
@@ -9,14 +9,27 @@ do
     curl -LSo flamecord.jar https://github.com/PanSzelescik/images/raw/main/java_test/flamecord.jar
 
     INTERNAL_LOBBY_PORT=$SERVER_PORT
-    env
-    echo "ZMIENNA $INTERNAL_LOBBY_PORT ZMIENNA"
 
-    if [ ! -f /home/container/dodatkowe-lobby/config.yml ]; then
-        echo -e "listeners:\r- host: 0.0.0.0:${INTERNAL_LOBBY_PORT}\r  query_enabled: true\r  query_port: ${INTERNAL_LOBBY_PORT}\r  motd: 'Serwer hostowany przez §aBedrock§fHost.pl'" > /home/container/dodatkowe-lobby/config.yml
+    if [ ! -f config.yml ]; then
+        echo -e "listeners:\r" >> config.yml
+        echo -e "- host: 0.0.0.0:${INTERNAL_LOBBY_PORT}\r" >> config.yml
+        echo -e "  query_enabled: true\r" >> config.yml
+        echo -e "  query_port: ${INTERNAL_LOBBY_PORT}\r" >> config.yml
+        echo -e "  motd: 'Serwer hostowany przez §aBedrock§fHost.pl\r" >> config.yml
+        echo -e "  force_default_server: true\r" >> config.yml
+        echo -e "  ping_passthrough: true\r" >> config.yml
+        echo -e "  forced_hosts: {}\r" >> config.yml
+        echo -e "forge_support: true\r" >> config.yml
+        echo -e "ip_forward: true\r" >> config.yml
+        echo -e "groups: {}\r" >> config.yml
+        echo -e "servers:\r" >> config.yml
+        echo -e "  lobby:\r" >> config.yml
+        echo -e "    motd: Serwer hostowany przez &aBedrock&fHost.pl\r" >> config.yml
+        echo -e "    address: localhost:25565\r" >> config.yml
+        echo -e "    restricted: false\r" >> config.yml
     else
-        sed -e "s/query_port: \([0-9]\+\)/query_port: ${INTERNAL_LOBBY_PORT}/g" -e "s/host: 0.0.0.0:\([0-9]\+\)/host: 0.0.0.0:${INTERNAL_LOBBY_PORT}/g" /home/container/dodatkowe-lobby/config.yml > /home/container/dodatkowe-lobby/bh_config.yml
-        mv /home/container/dodatkowe-lobby/bh_config.yml /home/container/dodatkowe-lobby/config.yml
+        sed -e "s/query_port: \([0-9]\+\)/query_port: ${INTERNAL_LOBBY_PORT}/g" -e "s/host: 0.0.0.0:\([0-9]\+\)/host: 0.0.0.0:${INTERNAL_LOBBY_PORT}/g" config.yml > bh_config.yml
+        mv bh_config.yml config.yml
     fi
 
     printf "\033[1m\033[33m[BedrockHost.pl - AntiBot]: \033[0mUruchamianie AntiBota...\n"
