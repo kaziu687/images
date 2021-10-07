8cd0375f8e865545951b0b19db5da0309690a585
PanSzelescik <panszelescik@gmail.com>
2021-10-07 19:46:00 +0200

    env

--- java_test/antibot.sh
+++ java_test/antibot.sh
@@ -9,12 +9,13 @@ do
     curl -LSo flamecord.jar https://github.com/PanSzelescik/images/raw/main/java_test/flamecord.jar
 
     INTERNAL_LOBBY_PORT=$SERVER_PORT
+    env
 
     if [ ! -f /home/container/dodatkowe-lobby/config.yml ]; then
         echo -e "listeners:\r- host: 0.0.0.0:${INTERNAL_LOBBY_PORT}\r  query_enabled: true\r  query_port: ${INTERNAL_LOBBY_PORT}\r  motd: 'Serwer hostowany przez §aBedrock§fHost.pl'" > /home/container/dodatkowe-lobby/config.yml
     else
-        sed -e "s/query_port: \([0-9]\+\)/query_port: $INTERNAL_LOBBY_PORT/g" -e "s/host: 0.0.0.0:\([0-9]\+\)/host: 0.0.0.0:$INTERNAL_LOBBY_PORT/g" config.yml > bh_config.yml
-        mv bh_config.yml config.yml
+        sed -e "s/query_port: \([0-9]\+\)/query_port: ${INTERNAL_LOBBY_PORT}/g" -e "s/host: 0.0.0.0:\([0-9]\+\)/host: 0.0.0.0:${INTERNAL_LOBBY_PORT}/g" /home/container/dodatkowe-lobby/config.yml > /home/container/dodatkowe-lobby/bh_config.yml
+        mv /home/container/dodatkowe-lobby/bh_config.yml /home/container/dodatkowe-lobby/config.yml
     fi
 
     printf "\033[1m\033[33m[BedrockHost.pl - AntiBot]: \033[0mUruchamianie AntiBota...\n"
