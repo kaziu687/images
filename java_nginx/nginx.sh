7b08d50901a2c0907c4933e02e00fd243b90717b
PanSzelescik <panszelescik@gmail.com>
2021-10-28 16:42:40 +0200

    Change

--- java_nginx/nginx.sh
+++ java_nginx/nginx.sh
@@ -1,5 +1,3 @@
 defined_envs=$(printf '${%s} ' $(env | cut -d= -f1))
-envsubst "$defined_envs" < "/utils/templates/nginx.conf.template" > "/etc/nginx/nginx.conf"
-cat /etc/nginx/nginx.conf
-cat /etc/nginx/conf.d/default.conf
-nginx -g 'daemon off;'
+envsubst "$defined_envs" < "/utils/templates/nginx.conf.template" > "/etc/nginx/sites-enabled/serwer-www"
+nginx -tg 'daemon off;'
