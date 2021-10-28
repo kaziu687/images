2ee135ab3689f16e3074741800daf5917f1df666
PanSzelescik <panszelescik@gmail.com>
2021-10-28 17:58:34 +0200

    Test

--- java_nginx/nginx.sh
+++ java_nginx/nginx.sh
@@ -1,4 +1,5 @@
-#defined_envs=$(printf '${%s} ' $(env | cut -d= -f1))
-#envsubst "$defined_envs" < "/utils/templates/nginx.conf.template" > "/etc/nginx/sites-enabled/serwer-www"
+defined_envs=$(printf '${%s} ' $(env | cut -d= -f1))
+echo defined_envs
+envsubst "$defined_envs" < "/utils/nginx.conf.template" > "/utils/nginx.conf"
 mkdir -p /home/container/_serwer_www/publiczny
 nginx -c '/utils/nginx.conf' -g 'daemon off;'
