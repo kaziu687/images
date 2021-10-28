2c60c7305b19f119848f5fa5310c41daa27f7b00
PanSzelescik <panszelescik@gmail.com>
2021-10-28 18:19:59 +0200

    /tmp

--- java_nginx/nginx.sh
+++ java_nginx/nginx.sh
@@ -1,5 +1,5 @@
 defined_envs=$(printf '${%s} ' $(env | cut -d= -f1))
 echo "$defined_envs"
-envsubst "$defined_envs" < "/utils/nginx.conf.template" > "/utils/nginx.conf"
+envsubst "$defined_envs" < "/utils/nginx.conf.template" > "/tmp/nginx.conf"
 mkdir -p /home/container/_serwer_www/publiczny
-nginx -c '/utils/nginx.conf' -g 'daemon off;'
+nginx -c '/tmp/nginx.conf' -g 'daemon off;'
