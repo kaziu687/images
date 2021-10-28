83d8ce74aa1296b940b59221a302bec2d2568e54
PanSzelescik <panszelescik@gmail.com>
2021-10-28 18:04:35 +0200

    Change

--- java_nginx/nginx.sh
+++ java_nginx/nginx.sh
@@ -1,5 +1,5 @@
 defined_envs=$(printf '${%s} ' $(env | cut -d= -f1))
-echo defined_envs
+echo "$defined_envs"
 envsubst "$defined_envs" < "/utils/nginx.conf.template" > "/utils/nginx.conf"
 mkdir -p /home/container/_serwer_www/publiczny
 nginx -c '/utils/nginx.conf' -g 'daemon off;'
