944a45df220dffc7e815ef0add2d38ec23ab28f1
PanSzelescik <panszelescik@gmail.com>
2021-10-28 16:38:10 +0200

    ?

--- java_nginx/nginx.sh
+++ java_nginx/nginx.sh
@@ -1,3 +1,5 @@
-bash /docker-entrypoint.sh
+defined_envs=$(printf '${%s} ' $(env | cut -d= -f1))
+envsubst "$defined_envs" < "/utils/templates/nginx.conf.template" > "/etc/nginx/nginx.conf"
+cat /etc/nginx/nginx.conf
 cat /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
