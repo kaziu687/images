add59ce404ebe9911773ea186c5007cf16ede3c1
PanSzelescik <panszelescik@gmail.com>
2021-10-28 17:31:27 +0200

    Fixes

--- java_nginx/nginx.sh
+++ java_nginx/nginx.sh
@@ -1,4 +1,4 @@
 #defined_envs=$(printf '${%s} ' $(env | cut -d= -f1))
 #envsubst "$defined_envs" < "/utils/templates/nginx.conf.template" > "/etc/nginx/sites-enabled/serwer-www"
-mkdir -p /home/container/_serwer-www/publiczny
+mkdir -p /home/container/_serwer_www/publiczny
 nginx -c '/utils/nginx.conf' -tg 'daemon off;'
