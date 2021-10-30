aaf2cc22b23b5b7b4091442f4b041a7a91f95a6c
PanSzelescik <panszelescik@gmail.com>
2021-10-30 16:18:48 +0200

    jq, parse port

--- java_nginx/nginx.sh
+++ java_nginx/nginx.sh
@@ -1,5 +1,12 @@
-defined_envs=$(printf '${%s} ' $(env | cut -d= -f1))
-echo "$defined_envs"
-envsubst "$defined_envs" < "/utils/nginx.conf.template" > "/tmp/nginx.conf"
 mkdir -p /home/container/_serwer_www/publiczny
+
+cp /utils/nginx.conf.template /tmp/nginx.conf
+
+if [ ! -f /home/container/_serwer_www/config.json ]; then
+    echo "{\"port\":30000}" > /home/container/_serwer_www/config.json
+fi
+
+port=jq '.port | tonumber' /home/container/_serwer_www/config.json
+sed -i "s/{PORT}/${port}/g" /tmp/nginx.conf
+
 nginx -c '/tmp/nginx.conf' -g 'daemon off;'
