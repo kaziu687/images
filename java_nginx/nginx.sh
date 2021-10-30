187396999b23beafd99b84c59d923f206b448815
PanSzelescik <panszelescik@gmail.com>
2021-10-30 16:23:23 +0200

    Fix

--- java_nginx/nginx.sh
+++ java_nginx/nginx.sh
@@ -6,7 +6,7 @@ if [ ! -f /home/container/_serwer_www/config.json ]; then
     echo "{\"port\":30000}" > /home/container/_serwer_www/config.json
 fi
 
-port=jq '.port | tonumber' /home/container/_serwer_www/config.json
+port=$(jq '.port | tonumber' /home/container/_serwer_www/config.json)
 sed -i "s/{PORT}/${port}/g" /tmp/nginx.conf
 
 nginx -c '/tmp/nginx.conf' -g 'daemon off;'
