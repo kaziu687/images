1013b4304d311536170c9992ca0b068935a1a64a
PanSzelescik <panszelescik@gmail.com>
2021-11-09 13:45:26 +0100

    Creating and merging json

--- java_nginx/utils/serwer_www/nginx.sh
+++ java_nginx/utils/serwer_www/nginx.sh
@@ -9,13 +9,17 @@ mkdir -p $serwer_www_path/publiczny
 
 cp /utils/nginx.conf.template /tmp/nginx.conf
 
-if [ ! -f "$config_path" ]; then
-    default_www_config="{\"_UWAGA\":\"Nie usuwaj oraz nie wprowadzaj zmian w tym pliku. Jeśli chcesz skonfigurować serwer WWW na swojej usłudze hostingu przejdź do ustawień.\",\"port\":30080,\"enabled\":true}"
-    jq -n --argjson default_www_config "$default_www_config" '{"serwer_www":$default_www_config}' > $config_path
+default_serwer_www="{\"_UWAGA\":\"Nie usuwaj oraz nie wprowadzaj zmian w tym pliku. Jeśli chcesz skonfigurować serwer WWW na swojej usłudze hostingu przejdź do ustawień.\",\"port\":30080,\"enabled\":true}"
+
+if [ ! -f "$config_path" ]; then # Jeśli nie ma pliku - utwórz go z domyślnymi ustawieniami serwera WWW
+    jq -n --argjson serwer_www "$default_serwer_www" '. + serwer_www:$serwer_www' > $config_path
+elif [ "$(jq '.serwer_www|type=="object"' "$config_path")" == "false" ]; then # Jeśli plik jest, ale serwer_www nie jest obiektem, dodaj do jsona domyślne ustawienia serwera WWW
+    jq --argjson serwer_www "$default_serwer_www" '. + serwer_www:$serwer_www' $config_path > /tmp/serwer_www.json
+    cp /tmp/serwer_www.json $config_path
 fi
 
 port=$(jq '.serwer_www.port | tonumber' "$config_path")
-if [[ -z "$port" || ${port} == 30080 ]]; then
+if [[ -z "$port" || $port == 30080 ]]; then
     printf "\033[1m\033[31m[Serwer WWW]: Port serwera nie został prawidłowo ustawiony, więc nie został on uruchomiony\033[0m\n"
     printf "\033[1m\033[31m[Serwer WWW]: Konfigurację serwera WWW znajdziesz w ustawieniach usługi\033[0m\n"
     printf "\033[1m\033[31m[Serwer WWW]: Jak uruchomić serwer WWW dowiesz się z naszego poradnika:\033[0m\n"
@@ -25,7 +29,7 @@ fi
 sed -i "s/{PORT}/$port/g" /tmp/nginx.conf
 
 enabled=$(jq '.serwer_www.enabled' "$config_path")
-if [[ "${enabled}" == "false" || "${enabled}" == "null" ]]; then
+if [[ -z "$enabled" || "$enabled" == "false" || "$enabled" == "null" ]]; then
     exit 0
 fi
 
