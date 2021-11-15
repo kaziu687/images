f356b3b5a049391d1e4b818276d06877bd111dca
PanSzelescik <panszelescik@gmail.com>
2021-11-15 14:26:56 +0100

    Fix

--- java_nginx/utils/main.sh
+++ java_nginx/utils/main.sh
@@ -7,7 +7,7 @@ uwaga="Nie usuwaj tego pliku oraz nie wprowadzaj w nim zmian. Ten plik jest gene
 if [ ! -f "$config_path" ]; then
     echo "Nie ma $settings_path"
     jq -nS --arg uwaga "$uwaga" '. + {_UWAGA:$uwaga}' > $config_path
-elif [ "$(jq '._UWAGA' "$config_path")" != "$uwaga" ]; then
+elif [ "$(jq '._UWAGA' "$config_path")" != "\"$uwaga\"" ]; then
     echo "Uwaga sie nie zgadza"
     jq '._UWAGA' "$config_path"
     jq -S --arg uwaga "$uwaga" '. + {_UWAGA:$uwaga}' $config_path > /tmp/bh_config.json
